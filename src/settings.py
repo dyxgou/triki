41a8c03c7b95dd847ecf44d837d536c82fa4720b
dyxgou <diego5614yt@gmail.com>
2024-09-21 17:19:39 -0500

    fix: Cambio de las settings para que reconozca las imagenes
    Ahora hay una nueva manera en la cual solicitamos las imagenes y las
    funetes, para tener todo mas centralizado

--- src/settings.py
+++ src/settings.py
@@ -1,9 +1,12 @@
 import pygame
+from pathlib import Path
+
+assets = Path("..") / "assets"
 
 HEIGHT = 700
 WIDTH = 1100
 
-BACKGROUND_PATH = "assets/Background.png"
+BACKGROUND_PATH = assets / "Background.png"
 TRICKYBUTTONSTART_PATH = "assets/StartButton.png"
 TRICKYBUTTONEXIT_PATH = "assets/ExitButton.png"
 TRICKYGRID_PATH = "assets/TrickyGrid.png"
@@ -11,5 +14,17 @@ TRICKYPLAYBUTTON_PATH = "assets/PlayButton.png"
 TRICKYX_PATH = "assets/TrickyX.png"
 TRICKYO_PATH = "assets/TrickyO.png"
 
+
 def get_font(size: int):
-    return pygame.font.Font("assets/font.ttf", size)
+    pygame.font.init()
+    font_path = assets / "font.ttf"
+    return pygame.font.Font(font_path, size)
+
+
+def get_image(image_name: str):
+    """
+    image_name : str -> El nombre de la imagen (Ejemplo : Background.png)
+    Ésta function retorna la imagen cargada en pygame.
+    """
+    image_path = assets / image_name
+    return pygame.image.load(image_path)
