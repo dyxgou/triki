e17dcb7ddfd6164e3e8c732c6420019281d8375f
dyxgou <diego5614yt@gmail.com>
2024-09-21 17:20:16 -0500

    fix: Adaptando la clase Screen a los nuevos cambios

--- src/screen.py
+++ src/screen.py
@@ -2,7 +2,7 @@ from pygame import SurfaceType
 import pygame
 from sys import exit
 
-from settings import BACKGROUND_PATH, HEIGHT, WIDTH
+from settings import HEIGHT, WIDTH, get_font, get_image
 from clock import Clock
 
 
@@ -15,9 +15,13 @@ class Screen(Clock):
     def __init__(self, title: str) -> None:
         Clock.__init__(self)
         self.__screen = pygame.display.set_mode((WIDTH, HEIGHT))
-        self.__background = pygame.image.load(BACKGROUND_PATH)
+        self.__background = get_image("Background.png")
         self.__screen.blit(self.__background, (0, 0))
         self.__title = title
+
+    @property
+    def screen(self):
+        return self.__screen
 
     def init(self):
         pygame.display.set_caption(self.__title)
@@ -29,9 +33,39 @@ class Screen(Clock):
                     exit()
 
             pygame.display.update()
-            self.tick(60)
+            self.tick(30)
 
 
 if __name__ == "__main__":
-    menu = Screen("ingeligencia artificiual!")
+    menu = Screen("Triki!")
+    surface = pygame.Surface((500, 500))
+    surface.fill((0, 0, 200))
+
+    font = get_font(18)
+    button_img = get_image("PlayButton.png")
+    text = font.render("start", True, "yellow")
+    text_center_cors = text.get_rect(
+        center=(
+            button_img.get_width() // 2,
+            button_img.get_height() // 2 - 10,
+        )
+    )
+
+    button_img.blit(text, text_center_cors)
+    center_cors = surface.get_rect(
+        center=(
+            menu.screen.get_width() // 2,
+            menu.screen.get_height() // 2,
+        )
+    )
+
+    surface.blit(
+        button_img,
+        (
+            0,
+            0,
+        ),
+    )
+
+    menu.screen.blit(surface, center_cors)
     menu.init()
