ff3d25196cd682e919b8e282500fe176c7a32119
dyxgou <diego5614yt@gmail.com>
2024-09-22 12:20:17 -0500

    fix: Ahora la clase Screen detecta el evento click

--- src/screen.py
+++ src/screen.py
@@ -1,9 +1,15 @@
-from pygame import SurfaceType
+from typing import Callable, Optional
+from pygame.event import Event
+from pygame import MOUSEBUTTONDOWN, SurfaceType
 import pygame
 from sys import exit
 
+
 from settings import HEIGHT, WIDTH, get_font, get_image
+from errors import screen_errors
 from clock import Clock
+
+EventHandler = Callable[[Event], None]
 
 
 class Screen(Clock):
@@ -11,6 +17,15 @@ class Screen(Clock):
     __title: str
     __background: SurfaceType
     __running: bool = True
+    __on_click: Optional[EventHandler] = None
+
+    @property
+    def on_click(self):
+        return self.__on_click
+
+    @on_click.setter
+    def on_click(self, new_on_click: EventHandler):
+        self.__on_click = new_on_click
 
     def __init__(self, title: str) -> None:
         Clock.__init__(self)
@@ -24,16 +39,20 @@ class Screen(Clock):
         return self.__screen
 
     def init(self):
+        if self.__on_click is None:
+            raise screen_errors.UndefinedClickError(
+                "El click handler no ha sido definido."
+            )
+
         pygame.display.set_caption(self.__title)
         while self.__running:
             for event in pygame.event.get():
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    pass
-
                 if event.type == pygame.QUIT:
                     self.__running = False
                     pygame.quit()
                     exit()
+
+                self.__on_click(event)
 
             pygame.display.update()
             self.tick(30)
@@ -41,6 +60,12 @@ class Screen(Clock):
 
 if __name__ == "__main__":
     menu = Screen("Triki!")
+
+    def on_click(event: Event):
+        if event.type == MOUSEBUTTONDOWN:
+            print("click!")
+
+    menu.on_click = on_click
     surface = pygame.Surface((500, 500))
     surface.fill((0, 0, 200))
 
