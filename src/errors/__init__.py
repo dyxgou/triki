664de3223b9647e59e1568b368668455a8257395
dyxgou <diego5614yt@gmail.com>
2024-09-22 12:16:31 -0500

    feat: Errores de pantalla
    En esta carpeta iran todos los errores del programa

---
+++ src/errors/__init__.py
@@ -0,0 +1,2 @@
+class UndefinedClickHandler(Exception):
+    pass
---
+++ src/errors/screen_errors.py
@@ -0,0 +1,2 @@
+class UndefinedClickError(Exception):
+    """Este error se lanza cuando el click no ha sido definido dentro de la la clase Screen."""
