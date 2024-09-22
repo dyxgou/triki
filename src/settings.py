b1ba310a8de404f6c5658a303e250da530bbac12
dyxgou <diego5614yt@gmail.com>
2024-09-22 13:12:54 -0500

    fix: Paths inecesarios eliminados y ancho y alto ajustados al fondo

--- src/settings.py
+++ src/settings.py
@@ -3,16 +3,8 @@ from pathlib import Path
 
 assets = Path("..") / "assets"
 
-HEIGHT = 700
-WIDTH = 1100
-
-BACKGROUND_PATH = assets / "Background.png"
-TRICKYBUTTONSTART_PATH = "assets/StartButton.png"
-TRICKYBUTTONEXIT_PATH = "assets/ExitButton.png"
-TRICKYGRID_PATH = "assets/TrickyGrid.png"
-TRICKYPLAYBUTTON_PATH = "assets/PlayButton.png"
-TRICKYX_PATH = "assets/TrickyX.png"
-TRICKYO_PATH = "assets/TrickyO.png"
+WIDTH = 1280
+HEIGHT = 853
 
 
 def get_font(size: int):
