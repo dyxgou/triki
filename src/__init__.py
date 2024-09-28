3a81e4518b223695753d4bbb68fc92d9b6edbd26
Brayan22-10 <castillobrayan851mail.com>
2024-09-27 21:45:11 -0500

    Documentacion del codigo __init__.py  carpeta src

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,11 @@
 from game import Game
 from screen import Screen
+
+"""
+Importaciones de las clases necesarias para el funcionamiento del juego.
+
+- `Game`: Esta clase gestiona el ciclo de vida del juego, incluyendo la lógica principal, la creación de superficies y el control de eventos. Se encarga de representar y gestionar los estados del juego.
+- `Screen`: Clase que representa una pantalla o ventana dentro del juego. Maneja elementos visuales, el renderizado, y gestiona las interacciones del usuario con la interfaz, como clics o teclas presionadas.
+
+Ambas clases son fundamentales para el manejo del juego y su interfaz gráfica.
+"""
