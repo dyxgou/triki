179625f9f8b22d800453137fac46bff310082f69
Brayan22-10 <castillobrayan851mail.com>
2024-09-27 21:47:58 -0500

    Documentacion del codigo __init__.py carpeta src/errors

--- src/errors/__init__.py
+++ src/errors/__init__.py
@@ -1,2 +1,16 @@
 class UndefinedClickHandler(Exception):
+    """
+    Excepción personalizada que se lanza cuando no se ha definido un manejador de clics.
+
+    Esta excepción puede utilizarse para indicar que una función o método que 
+    se esperaba que manejara eventos de clic no ha sido implementado o está ausente.
+
+    Ejemplo:
+        def handle_click(event):
+            if not click_handler:
+                raise UndefinedClickHandler("No se ha definido un manejador de clics.")
+
+    Hereda de:
+        Exception: La clase base para todas las excepciones en Python.
+    """
     pass
